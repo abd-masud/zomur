4ff1b076abae14061761fc02bb000694bb4c0e5f
abdMasud <abdmasud.2000@gmail.com>
2024-05-22 22:18:27 +0600

    Basic Setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="keywords" content="Site keywords here" />
+        <meta name="description" content="#" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>Zomur - Modern Bootstrap Dashboard Template</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,35 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="img/favicon.png" />
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/css/slick.min.css" />
+        <link rel="stylesheet" href="/css/font-awesome-all.min.css" />
+        <link rel="stylesheet" href="/css/charts.min.css" />
+        <link rel="stylesheet" href="/css/datatables.min.css" />
+        <link rel="stylesheet" href="/css/fancy-box.min.css" />
+        <link rel="stylesheet" href="/css/nice-select.min.css" />
+        <link rel="stylesheet" href="/css/pikaday.min.css" />
+        <link rel="stylesheet" href="/css/reset.css" />
+        <link rel="stylesheet" href="/style.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/jquery-migrate.js"></script>
+        <script src="/js/popper.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/charts.js"></script>
+        <script src="/js/final-countdown.min.js"></script>
+        <script src="/js/fancy-box.min.js"></script>
+        <script src="/js/fullcalendar.min.js"></script>
+        <script src="/js/datatables.min.js"></script>
+        <script src="/js/circle-progress.min.js"></script>
+        <script src="/js/nice-select.min.js"></script>
+        <script src="/js/pikaday.min.js"></script>
+        <script src="/js/main.js"></script>
       </body>
     </Html>
   );
